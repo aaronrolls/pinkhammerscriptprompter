e65a50eb4e3f2f11c48d85cf915c69036302d6ec
Aaron Rolls <public@aaronrolls.nz>
2023-07-21 14:14:52 +1200

    test

--- pink_hammer_web-8dd8e1419f8573f9.js
+++ pink_hammer_web-8dd8e1419f8573f9.js
@@ -969,7 +969,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('pink_hammer_web-69b26075173aada2_bg.wasm', import.meta.url);
+        input = new URL('pink_hammer_web-8dd8e1419f8573f9_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
